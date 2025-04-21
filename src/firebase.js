77bb42cac2e6a401fca01c3046a42d6a30670397
prathap443 <40711874+prathap443@users.noreply.github.com>
2025-04-21 17:46:24 +0100

    Add files via upload

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,16 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+// Use hardcoded values for testing
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "test-api-key",
+  authDomain: "test-project-id.firebaseapp.com",
+  projectId: "test-project-id",
+  storageBucket: "test-project-id.appspot.com",
+  messagingSenderId: "000000000000",
+  appId: "1:000000000000:web:0000000000000000000000"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
